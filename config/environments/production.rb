0b52865b6fed3e68259251c664e913c6df3912d2
Spencer Montgomery <smontgo5@gmail.com>
2013-05-03 10:00:34 -0700

    Edit Config Production Environment assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,9 +25,11 @@ Blog::Application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor  = :uglifier
   # config.assets.css_compressor = :sass
-
+  config.assets.compress = true
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+
 
   # Generate digests for assets URLs.
   config.assets.digest = true
